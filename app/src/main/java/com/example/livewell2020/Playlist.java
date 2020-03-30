e903b58fbf5966e8d39b4ae6fa13c755a483631a
rebecca0323 <rebeccaazhu@gmail.com>
2020-03-30 19:29:20 -0400

    bottom nav bar update

--- app/src/main/java/com/example/livewell2020/Playlist.java
+++ app/src/main/java/com/example/livewell2020/Playlist.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Playlist extends AppCompatActivity {
+
+    private BottomNavigationView bnav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
