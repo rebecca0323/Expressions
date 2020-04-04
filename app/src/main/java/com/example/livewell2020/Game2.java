65f3074d0802d6e3a732133a0852954e934ab149
rebecca0323 <rebeccaazhu@gmail.com>
2020-04-04 14:47:39 -0400

    camera is sideways

---
+++ app/src/main/java/com/example/livewell2020/Game2.java
@@ -0,0 +1,81 @@
+package com.example.livewell2020;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.camerakit.CameraKitView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.annotations.NotNull;
+
+public class Game2 extends AppCompatActivity{
+
+    private CameraKitView cameraKitView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        cameraKitView = findViewById(R.id.camera);
+
+        BottomNavigationView bnav = findViewById(R.id.bottom_nav_bar);
+        bnav.setSelectedItemId(R.id.game);
+
+        bnav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.settings:
+                        startActivity(new Intent(getApplicationContext(),
+                                Settings.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.music:
+                        startActivity(new Intent(getApplicationContext(),
+                                Playlist.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.game:
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cameraKitView.requestPermissions(Game2.this);
+        cameraKitView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraKitView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        cameraKitView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions, @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
