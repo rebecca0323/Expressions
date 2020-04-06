a56f364d0060d042ef009f1f509149cb166e07e1
rebecca0323 <rebeccaazhu@gmail.com>
2020-04-05 21:43:17 -0400

    cleaned up comments

--- app/src/main/java/com/example/livewell2020/Playlist.java
+++ app/src/main/java/com/example/livewell2020/Playlist.java
@@ -54,9 +54,7 @@ public class Playlist extends AppCompatActivity {
     public void zen(View view){
         Intent intent = new Intent(this, Song.class);
         Bundle bundle = new Bundle();
-        //String songURL = "https://firebasestorage.googleapis.com/v0/b/livewell2020-10b52.appspot.com/o/bensound-relaxing.mp3?alt=media&token=54a3197f-8695-4e96-8d57-57a121659e42";
         String songName = "Zen";
-        //bundle.putString("URL", songURL);
         bundle.putString("Name", songName);
         intent.putExtras(bundle);
         startActivity(intent);
@@ -65,9 +63,7 @@ public class Playlist extends AppCompatActivity {
     public void meditate(View view){
         Intent intent = new Intent(this, Song.class);
         Bundle bundle = new Bundle();
-        //String songURL = "https://firebasestorage.googleapis.com/v0/b/livewell2020-10b52.appspot.com/o/meditation.mp3?alt=media&token=f17388bc-f2f9-40c5-8883-65089ba52080";
         String songName = "Meditate";
-        //bundle.putString("URL", songURL);
         bundle.putString("Name", songName);
         intent.putExtras(bundle);
         startActivity(intent);
