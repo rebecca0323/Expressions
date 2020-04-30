b4878ef1cad04558cb93b48d86db8ad2e87e0aaf
rebecca0323 <rebeccaazhu@gmail.com>
2020-04-30 14:29:34 -0400

    adding lottie animation

---
+++ app/src/main/java/com/example/livewell2020/Congrats.java
@@ -0,0 +1,14 @@
+package com.example.livewell2020;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Congrats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_congrats);
+    }
+}
